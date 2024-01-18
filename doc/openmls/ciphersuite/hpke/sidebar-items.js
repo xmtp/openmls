503ea7d16c2ace36e185ad24ee18a8a8f42f3692
neekolas <neekolas@users.noreply.github.com>
2024-01-18 23:52:13 +0000

    deploy: a12c7435209b6714531bc0a1619f4fdf7c57d7c0

---
+++ doc/openmls/ciphersuite/hpke/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Error"],"fn":["decrypt_with_label","encrypt_with_label"],"struct":["EncryptContext"]};
--- doc/openmls/ciphersuite/sidebar-items.js
+++ doc/openmls/ciphersuite/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["hash_ref","signable","signature"],"struct":["HpkePrivateKey"],"type":["HpkePublicKey"]};
+window.SIDEBAR_ITEMS = {"mod":["hash_ref","hpke","signable","signature"],"struct":["HpkePrivateKey"],"type":["HpkePublicKey"]};
